87211c8e5a754d8a1705110f2cc9e68b15dc98db
Ramananda Samantaray <ramanandas@asiapac.altair.com>
2024-01-23 17:43:59 +0530

    Props & Components

--- 02tailwindprops/src/App.jsx
+++ 02tailwindprops/src/App.jsx
@@ -1,9 +1,14 @@
 import "./App.css";
+import Card from "./components/Card";
 
 function App() {
   return (
     <>
-      <h1 className="bg-green-400 text-black p-4 rounded-xl">Tailwind Test</h1>
+      <h1 className="bg-green-400 text-black p-4 rounded-xl mb-4">
+        Tailwind Test
+      </h1>
+      <Card name="Ram" btnText="Click me" />
+      <Card name="Shyam" />
     </>
   );
 }
